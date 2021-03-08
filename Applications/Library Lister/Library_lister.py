45dca93492d0b30c9d36e00945ff7fdc25efd146
FluxChikin <51963968+FluxChikin@users.noreply.github.com>
2021-03-08 09:17:05 -0700

    Worked on Library Lister, think I am going to start programming more

--- Applications/Library Lister/Library_lister.py
+++ Applications/Library Lister/Library_lister.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter.ttk import Label
-from tkinter import *
+from    tkinter import *
 import json as js
 
 # colors
@@ -10,7 +10,7 @@ darker_purple = "#%02x%02x%02x" % (55, 0, 188)
 cyan = "#%02x%02x%02x" % (3, 218, 198)
 
 # configuring main page window
-home = tk.Tk()
+home = Tk()
 home.attributes("-fullscreen", True)
 home.configure(bg=light_grey)
 
@@ -18,63 +18,99 @@ home.configure(bg=light_grey)
 height = home.winfo_screenheight()
 width = home.winfo_screenwidth()
 
-# string variables
+# variables
 entry_var = tk.StringVar()
-
-# functions
-def enter(filename):
-    file = open(filename, "r")
-    data = js.load(file)
-    return data
-
-def output(dict, file):
-    with open(file, "w") as f:
-        js.dump(dict, f)
-
-def add_entry():
-    list_box1.insert(tk.END, entry_var.get())
+f1 = Frame(home)
+Home = Frame(home)
 
 
+class setup:
 
-# title
-title = tk.Label(home, text="Calvin Webb's", font=("Arial", 100), fg=light_purple, bg=light_grey)
-title.pack()
-title.update()
-title.place(x = width/2-(title.winfo_width()/2),y=0)
-title_sub = tk.Label(home, text="Library Encyclopedia", font=("Arial", 50), fg=light_purple, bg=light_grey)
-title_sub.pack()
-title_sub.update()
-title_sub.place(x = width/2-(title_sub.winfo_width()/2),y=title.winfo_height()+5)
+    def __init__(self):
+        title = tk.Label(home, text="Calvin Webb's", font=("Arial", 100), fg=light_purple, bg=light_grey)
+        title.pack()
+        title.update()
+        title.place(x = width/2-(title.winfo_width()/2),y=0)
+        title_sub = tk.Label(home, text="Library Encyclopedia", font=("Arial", 50), fg=light_purple, bg=light_grey)
+        title_sub.pack()
+        title_sub.update()
+        title_sub.place(x = width/2-(title_sub.winfo_width()/2),y=title.winfo_height()+5)
 
-# interactables
-button1 = tk.Button(home, text="Home", font=("Arial", 25), bd=5, bg=light_purple, fg=darker_purple)
-button1.place(x=10,y=10)
-button2 = tk.Button(home, command=add_entry, text="Add Entry", font=("Arial", 25), bd=5, bg=light_purple, fg=darker_purple)
-button2.pack()
-button2.update()
-button2.place(x=width-(button2.winfo_width()+10),y=10)
-languages = ["Python", "Java", "C++", "HTML"]
-list1 = tk.StringVar(home)
-list1.set(languages[0])
-drop_down1 = tk.OptionMenu(home, list1, *languages)
-drop_down1["menu"].config(bg=light_purple)
-drop_down1["highlightthickness"]=0
-drop_down1.config(bg=light_purple)
-drop_down1.pack()
-drop_down1.update()
-drop_down1.place(x=width-(button2.winfo_width()+710), y=500)
-Category = ["Math", "GUI", "NN/AI", "Optimization"]
-list2 = tk.StringVar(home)
-list2.set(Category[0])
-drop_down2 = tk.OptionMenu(home, list2, *Category)
-drop_down2["menu"].config(bg=light_purple)
-drop_down2["highlightthickness"]=0
-drop_down2.config(bg=light_purple)
-drop_down2.pack()
-drop_down2.update()
-drop_down2.place(x=width-(button2.winfo_width()+780), y=500)
+    def enter(self, filename):
+        file = open(filename, "r")
+        data = js.load(file)
+        return data
+
+    def output(self, dict, file):
+        with open(file, "w") as f:
+            js.dump(dict, f)
+
+    def add_entry(self):
+        list.list_box1.insert(tk.END, entry_var.get())
+
+    def raise_frame(self, frame):
+        frame.tkraise()
+
+set = setup()
+
 entry1 = tk.Entry(home, width=116, textvariable=entry_var)
 entry1.place(x=900,y=500,height=25)
-list_box1 = tk.Listbox(home)
-list_box1.place(x=900, y=530, width=850, height=height-530)
+
+class list_boxes:
+
+    def __init__(self):
+        self.list_box1 = tk.Listbox(home)
+        self.list_box1.place(x=900, y=530, width=850, height=height-530)
+        self.list_box1.update()
+list = list_boxes()
+
+
+class buttons:
+    def __init__(self):
+        self.button1 = tk.Button(home, text="Home", font=("Arial", 25), bd=5, bg=light_purple, fg=darker_purple)
+        self.button1.place(x=10,y=10)
+        self.button2 = tk.Button(home, command=set.add_entry(), text="Add Entry", font=("Arial", 25), bd=5, bg=light_purple, fg=darker_purple)
+        self.button2.pack()
+        self.button2.update()
+        self.button2.place(x=width-(self.button2.winfo_width()+10),y=10)
+
+button = buttons()
+
+class drop_downs:
+
+    def __init__(self):
+        self.languages = ["Python", "Java", "C++", "HTML"]
+        self.list1 = tk.StringVar(home)
+        self.list1.set(self.languages[0])
+        self.Category = ["Math", "GUI", "NN/AI", "Optimization"]
+        self.list2 = tk.StringVar(home)
+        self.list2.set(self.Category[0])
+
+    def drop_down1(self):
+        self.drop_down1 = tk.OptionMenu(home, self.list1, *self.languages)
+        self.drop_down1["menu"].config(bg=light_purple)
+        self.drop_down1["highlightthickness"]=0
+        self.drop_down1.config(bg=light_purple)
+        self.drop_down1.pack()
+        self.drop_down1.update()
+        self.drop_down1.place(x=width-(button.button2.winfo_width()+710), y=500)
+
+    def drop_down2(self):
+        self.drop_down2 = tk.OptionMenu(home, self.list2, *self.Category)
+        self.drop_down2["menu"].config(bg=light_purple)
+        self.drop_down2["highlightthickness"]=0
+        self.drop_down2.config(bg=light_purple)
+        self.drop_down2.pack()
+        self.drop_down2.update()
+        self.drop_down2.place(x=width-(button.button2.winfo_width()+780), y=500)
+
+
+
+
+print("got past")
+selection = list.list_box1.curselection()
+drop = drop_downs()
+drop.drop_down1()
+drop.drop_down2()
+f1.tkraise()
 tk.mainloop()
